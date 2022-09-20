20161b032931abb07c88033ddc2d4003ba46e1fe
Najmus Sakib <nazmusakib412@gmail.com>
2022-09-21 03:07:14 +0600

    Add(storybook): testing my first component into storybooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
-function Editor() {
-  return <div>Editor</div>;
+import { FC } from "react";
+
+export interface EditorProps {
+  text: string
+}
+
+const Editor: FC<EditorProps> = ({ text }) => {
+  return <div>{text}</div>;
 }
 
 export default Editor;
---
+++ src/stories/Editor.stories.tsx
@@ -0,0 +1,24 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Editor from '..';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Example/Editor',
+    component: Editor,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        text: 'Hello'
+    },
+} as unknown as ComponentMeta<typeof Editor>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Editor> = (args) => <Editor {...args} />;
+
+export const Main = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Main.args = {
+    text: 'Editor'
+};
+
+
