3ae067c755d89fd315382743a3f0192d169b62c3
Najmus Sakib <nazmusakib412@gmail.com>
2022-09-21 04:08:03 +0600

    Add(lexical): main editor with plain text

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,49 @@
-import { FC } from "react";
+import { FC } from 'react';
+
+import { $getRoot, $getSelection, EditorState } from 'lexical';
+
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
+import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
 
 export interface EditorProps {
-  text: string
+  placeholder: string;
 }
 
-const Editor: FC<EditorProps> = ({ text }) => {
-  return <div>{text}</div>;
+const theme = {};
+
+function onChange(editorState: EditorState) {
+  editorState.read(() => {
+    // Read the contents of the EditorState here.
+    const root = $getRoot();
+    const selection = $getSelection();
+
+    console.log(root, selection);
+  });
 }
+
+function onError(error: any) {
+  console.error(error);
+}
+
+const Editor: FC<EditorProps> = ({ placeholder }) => {
+  const initialConfig = {
+    namespace: 'LexicalEditor',
+    theme,
+    onError,
+  };
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>{placeholder}</div>}
+      />
+      <OnChangePlugin onChange={onChange} />
+      <HistoryPlugin />
+    </LexicalComposer>
+  );
+};
 
 export default Editor;
--- stories/Editor.stories.tsx
+++ stories/Editor.stories.tsx
@@ -1,15 +1,16 @@
+import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import Editor from '..';
+import Editor from '../src';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: 'Example/Editor',
-    component: Editor,
-    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-    argTypes: {
-        text: 'Hello'
-    },
+  title: 'Example/Editor',
+  component: Editor,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    placeholder: 'Hello',
+  },
 } as unknown as ComponentMeta<typeof Editor>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -18,7 +19,5 @@ const Template: ComponentStory<typeof Editor> = (args) => <Editor {...args} />;
 export const Main = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Main.args = {
-    text: 'Editor'
+  placeholder: 'Editor',
 };
-
-
