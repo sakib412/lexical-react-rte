b7a7ccd3c44e586125cb9bc0e36b36dd2e054816
Najmus Sakib <nazmusakib412@gmail.com>
2023-11-13 22:23:19 +0600

    Add: build configuration for library export.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import {resolve} from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/Editor.tsx'),
+      name: 'LexicalReactRTE',
+      fileName: 'lexical-react-rte',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
